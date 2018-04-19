3b75a85ea86629209f935dc3d9eef6761095fdf7
Felipy Cabral <lipycabral@gmail.com>
2018-04-19 14:12:10 -0500

    Alteração nos arquivos aula 12/04

--- aula01.cpp
+++ aula01.cpp
@@ -1,4 +1,6 @@
+
 #include<stdio.h>
+//fibonacci sem recursividade
 void fibbonnaci(int n){
     int pV = 0, sV = 1 , tV, aux;
     while(pV < n){
@@ -8,6 +10,31 @@ void fibbonnaci(int n){
         sV = tV;
     }
     printf("%d", pV); 
+}
+//fibonacci com recursividade
+int fibonacciTeste(int n){
+    int fib;
+    if(n == 0){
+        return 0;
+    }
+    else if(n == 1){
+        return 1;
+    }
+    else {
+        fib = (fibonacciTeste(n-1)+fibonacciTeste(n-2));
+        return fib;
+    }
+}
+//fatorial com recursividade
+int fatorial(int n){
+    int fat;
+    if(n==0){
+        return 1;
+    }
+    else{
+        fat = n*fatorial(n-1);
+        return fat;
+    }
 }
 int retornaMaior(int vetor[], int n){
     int maior = vetor[0];
@@ -29,11 +56,18 @@ main(){
     int n;
     printf("Digite o valor de fibbonnaci: ");
     scanf("%d", &n);
-    fibbonnaci(n);
+    //fibbonnaci(n);
     printf("\n---------------------------\n");
-    int vetor[] = {10,9,8,7,6,5,4,3,2,1};
-    printf("Maior número do vetor %d", retornaMaior(vetor, 10));
+    for(int i = 0 ; i < 10 ; i++)
+        printf("%d ",fibonacciTeste(i));
     printf("\n---------------------------\n");
-    printf("Menor número do vetor %d", retornaMenor(vetor, 10));
     printf("\n---------------------------\n");
+    for(int i = 0 ; i < 10 ; i++)
+        printf("%d ",fatorial(i));
+    printf("\n---------------------------\n");
+    //int vetor[] = {10,9,8,7,6,5,4,3,2,1};
+    //printf("Maior número do vetor %d", retornaMaior(vetor, 10));
+    //printf("\n---------------------------\n");
+    //printf("Menor número do vetor %d", retornaMenor(vetor, 10));
+    //printf("\n---------------------------\n");
 }
